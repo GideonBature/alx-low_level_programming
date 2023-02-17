d7008e933ff3052621a08b74911f1211738b9e5a
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-16 20:19:36 -0800

    corrected comment for return type based on betty style of coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - entry point
- *return: always 0 (success)
- */
+*main - Entry point
+*Return: always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
