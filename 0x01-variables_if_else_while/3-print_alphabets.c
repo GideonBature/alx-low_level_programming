b98fa9296c8f18e61ad2965540928d8c4fe87703
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-17 18:55:27 -0800

    fix betty tab indent

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                putchar(ch);
-        }
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
