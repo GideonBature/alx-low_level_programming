45d74c6ee663ebc1388c574f4873e2ecb2a36ee8
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-22 05:10:02 -0800

    a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -28,7 +28,7 @@ void jack_bauer(void)
 			{
 				_putchar(':');
 				_putchar('0');
-				_putchar(j);
+				_putchar(j + '0');
 			}
 			else if (j > 10)
 			{
@@ -36,9 +36,6 @@ void jack_bauer(void)
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
-			_putchar(i);
-			_putchar(':');
-			_putchar(j);
 		}
 	}
 }
