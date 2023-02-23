86e44cd2948fc974796e0271acd30205b4009fc6
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-23 09:35:12 -0800

    a test for a postitive_or_negative() function

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests func that prints +ve/-ve integer
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
