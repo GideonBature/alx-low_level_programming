8e8c16f01959cda39525b847a7f51456b88403ee
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-22 17:19:23 -0800

    refactor function to be just 40 lines of code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,47 +5,36 @@
 */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int k;
-
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+	for (int i = 0; i <= n; ++i)
 	{
-		for (i = 0; i <= n; i++)
+		for (int j = 0; j <= n; ++j)
 		{
-			for (j = 0; j <= n; j++)
+			int k = i * j;
+			if (j != 0)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				_putchar(','), _putchar(' ');
 			}
-			_putchar('\n');
+			if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k < 100)
+			{
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			else
+			{
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
