9433234ea2992fdf002984d2e7929eabcc330692
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-22 17:02:41 -0800

    a program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+/*
+ * File: 101-natural.c
+ * Auth: Gideon Bature
+*/
+#include <stdio.h>
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
