7fec68f4c6bdba2989ec240885729f1046cf566c
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-22 18:19:37 -0800

    adjust number of characters in line 23

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,10 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
+				}else if (k < 10 && j != 0)
 				{
-					_putchar(','), _putchar(' '), _putchar(' '), _putchar(' '), _putchar(k + '0');
+					_putchar(','), _putchar(' '), _putchar(' '), _putchar(' ');
+					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
