6e2dcbe6b473e46186409f0546e493bd625fd5de
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-22 18:16:19 -0800

    adjust lines

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,34 +5,36 @@
 */
 void print_times_table(int n)
 {
-	int i, j;
-	if (n < 0 || n > 15)
-		return;
-	for (i = 0; i <= n; i++)
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			int k = i * j;
-			if (j != 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(','), _putchar(' ');
+				k = i * j;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(','), _putchar(' '), _putchar(' '), _putchar(' '), _putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(','), _putchar(' '), _putchar(' ');
+					_putchar((k / 10) + '0'), _putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(','), _putchar(' ');
+					_putchar((k / 100) + '0'), _putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			if (k < 10)
-			{
-				_putchar(' '), _putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k < 100)
-			{
-				_putchar(' '), _putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			else
-			{
-				_putchar((k / 100) + '0'), _putchar(((k / 10) % 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
