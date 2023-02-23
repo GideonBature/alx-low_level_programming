51d881c32b88f6a75e6fe0fc62abbc67ffcf14d4
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-22 17:56:12 -0800

    change pre increment to post increment

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,9 +8,9 @@ void print_times_table(int n)
 	int i, j;
 	if (n < 0 || n > 15)
 		return;
-	for (i = 0; i <= n; ++i)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; ++j)
+		for (j = 0; j <= n; j++)
 		{
 			int k = i * j;
 			if (j != 0)
