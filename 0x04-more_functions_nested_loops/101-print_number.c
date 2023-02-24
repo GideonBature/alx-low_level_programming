1bd528f166fe02e3fbca4c4bb00b7b72ed8f5d95
Gideon Bature <infoaboutgideon@gmail.com>
2023-02-23 19:35:23 -0800

    adjust to suite betty style

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - prints an integer
  * @n: integer to be printed
@@ -11,8 +11,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		_putchar('-');
-	} else
+		putchar('-');
+	}
+	else
 	{
 		n1 = n;
 	}
@@ -22,5 +23,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 
-	_putchar((n1 % 10) + '0');
+	putchar((n1 % 10) + '0');
 }
