e04cba06b1e925b4d2403df8b53a0192c5550499
Bene Elohim Hub <gideonbature5@gmail.com>
2023-03-10 16:06:06 -0800

    update to pass betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	num = atoi(argv[1]);
-	
 	result = 0;
 	if (num < 0)
 	{
