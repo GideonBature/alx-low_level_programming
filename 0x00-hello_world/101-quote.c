f629a2210a2f34e4ea9870b8ec063b0fdaeabe75
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-18 15:39:12 -0700

    correct 26 to 28 for num of chars to be printed on the second fwrite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	fwrite("and that piece of art is useful\"", 1, 31, stdout);
-	fwrite(" - Dora Korpar, 2015-10-19\n", 1, 26, stdout);
+	fwrite(" - Dora Korpar, 2015-10-19\n", 1, 28, stdout);
 
 	return (1);
 }
