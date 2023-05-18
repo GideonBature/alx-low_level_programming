293368e5d9c459f91edf6d5d3aa278bf24483d9f
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-18 15:43:53 -0700

    adjust each line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\"", 1, 31, stdout);
-	fwrite(" - Dora Korpar, 2015-10-19\n", 1, 28, stdout);
+	fwrite("and that piece of art is useful\" - ", 1, 35, stdout);
+	fwrite("Dora Korpar, 2015-10-19\n", 1, 24, stdout);
 
 	return (1);
 }
