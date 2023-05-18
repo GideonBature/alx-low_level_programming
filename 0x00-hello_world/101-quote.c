ba4c77852b07959988cda1669a64cb757880efe3
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-18 15:31:12 -0700

    correct num of chars to 59

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 57, stdout);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stdout);
 
 	return (1);
 }
