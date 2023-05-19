4d5a52e04d7843c8c466ef7a953223856bb95c5a
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-19 11:15:00 -0700

    add missing quotes to numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k = j + 1; k <= 9; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
 				putchar(i);
 				putchar(j);
