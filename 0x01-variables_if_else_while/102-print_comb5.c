c11a3b56e292ff66399a488c5998ed9c19be6f11
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-19 12:49:02 -0700

    add comma and space

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,6 +16,12 @@ int main(void)
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
+			
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
