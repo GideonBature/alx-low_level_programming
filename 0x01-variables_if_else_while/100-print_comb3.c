1babbea507e19e1090f554cbc6702d8efbbe611c
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-19 09:56:02 -0700

    prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible diff combination of 2 digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	fwrite('0', 1, 1, stdout);
+	fwrite('1', 1, 1, stdout);
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '2'; j <= '9'; j++)
+		{
+			if ((i != j) && (i != (j+1)) && (i != (j+2)) && (i != (j+3)))
+			{
+				if ((i != (j+4)) && (i != (j+5)) && (i != (j+6)) && (i != (j+7)))
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(i);
+					putchar(j);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
