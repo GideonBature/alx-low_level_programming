94a73751f29384a2e553cb5835e38fb1900e7067
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-23 14:41:49 -0700

    fix betty white trailing space and comma and space for nums greater than 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-			
+
 			if (i < 98)
 			{
 				printf(",");
@@ -27,8 +27,8 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			
-			if (i < 98)
+
+			if (i > 98)
 			{
 				printf(",");
 				printf(" ");
