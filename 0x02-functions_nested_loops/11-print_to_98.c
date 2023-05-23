376a7a01af1c09cd182d02d6403862dada71a534
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-23 14:38:01 -0700

    consider the numbers greater than 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,20 +2,37 @@
 #include <stdio.h>
 /**
  * print_to_98 - prints from n to 98
+ * @n: parameter
  * Return: 0
  */
 void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		printf("%d", i);
-
-		if (i < 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf(",");
-			printf(" ");
+			printf("%d", i);
+			
+			if (i < 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			
+			if (i < 98)
+			{
+				printf(",");
+				printf(" ");
+			}
 		}
 	}
 	printf("\n");
