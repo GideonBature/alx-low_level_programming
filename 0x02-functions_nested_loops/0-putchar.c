1715a4f21c4d7e1aa3a3e7ca1e4c686597554776
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-23 04:55:12 -0700

    correct output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,13 @@
  */
 int main(void)
 {
-	char ch;
-
-	ch = 'a';
-
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 }
