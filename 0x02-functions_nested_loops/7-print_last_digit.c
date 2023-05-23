8f1d7ed33bf25739fdf512326d52e21c4538ca28
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-23 13:40:02 -0700

    use putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,13 @@ int print_last_digit(int num)
 	if (num < 0)
 	{
 		last = ((-1 * num) % 10);
-		_putchar(last);
-		return (last);
+		_putchar(last + '0');
+		return (last + '0');
 	}
 	else
 	{
 		last =  (num % 10);
-		_putchar(last);
-		return (last);
+		_putchar(last + '0');
+		return (last + '0');
 	}
 }
