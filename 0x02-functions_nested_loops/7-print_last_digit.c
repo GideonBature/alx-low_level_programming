084657c2e1ecd33a390d11390cc7cef24e9a0290
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-23 12:18:32 -0700

    add printf to the return

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_last_digit - prints last digit of any number
  * @num: parameter
@@ -8,10 +9,12 @@ int print_last_digit(int num)
 {
 	if (num < 0)
 	{
+		printf("%d", (-1 * num) % 10);
 		return ((-1 * num) % 10);
 	}
 	else
 	{
+		printf("%d", num % 10);
 		return (num % 10);
 	}
 }
