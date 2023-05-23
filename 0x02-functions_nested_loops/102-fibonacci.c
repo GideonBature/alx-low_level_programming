22b84b21481d3c066056ad97e8fcce9474d45a14
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-23 15:43:26 -0700

    correct to just 50 series instead of 51

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	long int i, n1 = 0, n2 = 1, sum = 0;
 
-	for (i = 0; i <= 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		sum = n1 + n2;
 
@@ -15,7 +15,7 @@ int main(void)
 		n2 = sum;
 
 		printf("%ld", sum);
-		if (i < 50)
+		if (i < 49)
 		{
 			printf(",");
 			printf(" ");
