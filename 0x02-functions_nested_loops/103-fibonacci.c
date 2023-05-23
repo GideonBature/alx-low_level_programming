f3c987d694be2af9da0736a16cb8494e2f848e5a
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-23 16:21:21 -0700

    a program that finds and prints the sum of the even-valued terms, followed by a new line.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - adds even numbers of fibonacci series 1 and 2
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, num1 = 1, num2 = 2, num3 = 0, sum = 0;
+
+	for (i = 0; i < 30; i++)
+	{
+		if (num2 % 2 == 0 && num2 < 4000000)
+		{
+			sum += num2;
+		}
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
