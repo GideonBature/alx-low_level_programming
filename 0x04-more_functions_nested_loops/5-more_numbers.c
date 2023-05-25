ef9cc9a017f3db08074e827bb895ad1596831b44
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-25 08:07:07 -0700

    improve code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 0 && j < 10)
+			if (j >= 0 && j <= 9)
 			{
 				_putchar(j + '0');
 			}
