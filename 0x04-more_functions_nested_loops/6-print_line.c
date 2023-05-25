54d7c71264c1534ace5f1ea567d96b4fdf2a0001
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-25 08:38:15 -0700

    adjust for loop to start from 1 to meet upto conditions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,9 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
-	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+	for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
-	}
 	_putchar('\n');
 }
