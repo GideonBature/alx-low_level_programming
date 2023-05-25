bf3f4406d4c6a81ca51ce432dde0921bb74ac780
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-25 09:40:59 -0700

    testing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,14 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 0 && j <= 9)
-			{
-				_putchar(j + '0');
-			}
-			else
+			if (j >= 10 && j <= 14)
 			{
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
+			}
+			else
+			{
+				_putchar(j + '0');
 			}
 		}
 		_putchar('\n');
