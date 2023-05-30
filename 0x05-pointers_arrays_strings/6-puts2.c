99d22b5e9f97afc2080a965541fa528daa0868a2
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-29 20:39:39 -0700

    correct < to != in second for loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,16 @@ void puts2(char *str)
 {
 	int i;
 
-	if (str[0] == 'H')
+	if (str[0] != 'H')
 	{
-		for (i = 0; str[i] != '\0'; i+=2)
+		for (i = 0; (str[i] != '\0') && (str[i] != 'H'); i+=2)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = 0; str[i] < '\0' && str[i] != 'H'; i+=2)
+		for (i = 0; str[i] != '\0'; i+=2)
 		{
 			_putchar(str[i]);
 		}
