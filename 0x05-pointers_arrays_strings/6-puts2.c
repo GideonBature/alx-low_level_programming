ee42b5d522029f5c70831cae9dc9180c530a9724
Gideon Bature <infoaboutgideon@gmail.com>
2023-05-29 19:15:23 -0700

    fix for longest text

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] < '\0'; i += 2)
+	for (i = 0; str[i] < '\0'; i++)
 	{
-		_putchar(str[i]);
+		if (str[i] != '\0')
+		{
+			_putchar(str[i+1]);
+		}
 	}
 	_putchar('\n');
 }
