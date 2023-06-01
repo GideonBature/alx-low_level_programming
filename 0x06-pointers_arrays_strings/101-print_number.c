92232b1a9705858fc2e921d81c542d337d97ce2d
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-01 13:25:32 -0700

    add for 4 digits

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -34,4 +34,12 @@ void print_number(int n)
 		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
+	else if (n >= 10000 && n <= 99999)
+	{
+		_putchar(n / 10000 + '0');
+		_putchar((n / 1000) % 10 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 }
