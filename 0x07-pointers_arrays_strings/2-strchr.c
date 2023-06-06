c4ece57c274fb9bb11f36d481663d06c0b4e7047
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-06 05:35:18 +0100

    use pointer to loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for ( ; *s != '\0'; s++)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (&s[i]);
+			return (s);
 		}
 	}
 	return (NULL);
