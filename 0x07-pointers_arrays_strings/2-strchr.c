b4fee3adad3e00954dd54ccefbb037191f22d443
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-06 02:53:51 +0100

    return address to the sring

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	for ( ; *s != '\0'; s++)
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (&s[i]);
 		}
 	}
 	return (NULL);
