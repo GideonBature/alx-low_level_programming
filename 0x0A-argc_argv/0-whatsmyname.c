b467b1741e423c3423fb201ab7344a26ac45d3ae
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-09 18:10:21 +0100

    a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints programs name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	char *name;
+	name = argv[0];
+
+	for (i = 0; name[i] != '\0' && argc == 1; i++)
+	{
+		_putchar(name[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,1 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
 int _putchar(char c);
-
-#endif
