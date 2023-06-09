a363383b9df06ca5c66a7eeed61c7cc2bf2e8bff
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-09 22:17:07 +0100

    correct betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,8 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < argc; i++)
 	{
-		if (*argv[i] >= 'a' && *argv[i] <= 'z' || *argv[i] >= 'A' && *argv <= 'Z')
+		if ((*argv[i] >= 'a' && *argv[i] <= 'z') || 
+				(*argv[i] >= 'A' && *argv[i] <= 'Z'))
 		{
 			printf("Error\n");
 			return (1);
