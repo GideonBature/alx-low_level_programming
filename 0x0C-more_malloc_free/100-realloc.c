f2ef23ab24ffba8b387a821bbb7df2dc7d60584b
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-16 05:53:48 +0100

    considered shrinking the memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,6 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
-	unsigned int min_size;
 
 	new_ptr = malloc(new_size);
 
@@ -39,10 +38,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
+	if (new_size < old_size)
+	{
+		return (new_ptr);
 
-	min_size = (old_size < new_size) ? old_size : new_size;
-	memcpy(new_ptr, ptr, min_size);
-	free(ptr);
-
+		if (new_ptr != NULL)
+		{
+			memcpy(new_ptr, ptr, new_size);
+		}
+		free(ptr);
+		return (new_ptr);
+	}
 	return (new_ptr);
 }
