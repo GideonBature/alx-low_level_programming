9b40f8fe40bc0b4269fa7483a38d43b0372f7e2e
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-16 07:38:03 +0100

    collapse two ifs statement into one

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,10 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
-		return (new_ptr);
-
-	if (new_size > old_size)
+	if (ptr == NULL || new_size > old_size)
 		return (new_ptr);
 
 	if (new_size == old_size)
