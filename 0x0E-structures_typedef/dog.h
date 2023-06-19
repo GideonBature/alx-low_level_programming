55143ef3eaea17815fa081e88db520c46e29bebb
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-19 21:28:52 +0100

    defined a struct dog with name, age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - struct of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: sets name, age and owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
