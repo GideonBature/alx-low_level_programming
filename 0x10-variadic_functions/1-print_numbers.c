5bb9ec1e5b8112f6faa57cc584bb21010352b689
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-22 21:53:45 +0100

    use the add operator for two logic in one if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,12 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(num, unsigned int));
-		if (i < n - 1)
+		if (i < n - 1 && separator != NULL)
 		{
-			if (separator == NULL)
-			{
-				return;
-			}
 			printf("%c ", *separator);
 		}
 	}
