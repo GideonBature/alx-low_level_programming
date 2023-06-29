152919602c1c3c1c30784fa503d59f0ccc912068
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-29 15:14:36 +0100

    code from scratch

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_char - prints character
+ * @arg: argument
+ * Return: void
+*/
+void print_char(va_list arg)
+{
+	printf("%c", (unsigned char) va_arg(arg, int));
+}
+
+/**
+ * print_int - prints integer
+ * @arg: argument
+ * Return: void
+*/
+void print_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * print_float - prints float
+ * @arg: argument
+ * Return: void
+*/
+void print_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * print_str - prints string
+ * @arg: argument
+ * Return: void
+*/
+void print_str(va_list arg)
+{
+	while (!arg)
+	{
+		printf("(nill)");
+		return;
+	}
+	printf("%s", va_arg(arg, char *));
+}
+
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+
+	int i = 0, j;
+
+	print_t print_func[] = {
+		{ 'c', print_char },
+		{ 'i', print_int },
+		{ 'f', print_float },
+		{ 's', print_str },
+	};
+
+	va_start(arg, format);
+
+	while (format[i])
+	{
+		for (j = 0; j < 4; j++)
+		{
+			if (format[i] == print_func[j].alphabet)
+			{
+				print_func[j].print(arg);
+				if (j < 3)
+				{
+					printf(", ");
+				}
+			}
+		}
+		i++;
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,16 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct print_cifs
+{
+	char alphabet;
+	void (*print)(va_list arg);
+} print_t;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
