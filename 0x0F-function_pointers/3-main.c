08888633af868c9c44dbd162240176632a6ac2cc
Gideon Bature <infoaboutgideon@gmail.com>
2023-06-29 13:21:45 +0100

    fix betty

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,34 +11,33 @@
 */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*get_op_func(char *s))(int, int);
+	int num1, num2, result;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	int (*get_op_func(char *s))(int, int);
 
-    if (get_op_func(argv[2]) == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    result = get_op_func(argv[2])(num1, num2);
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    printf ("%d\n", result);
+	result = get_op_func(argv[2])(num1, num2);
 
-    return (0);
-
+	printf("%d\n", result);
+	return (0);
 }
