2a88b3e0dfa31f3c54e5df5a93d4b671f0958eaf
Gideon Bature <infoaboutgideon@gmail.com>
2023-07-06 01:13:03 +0100

    add usecase for idx at 0 index and made changes to structure of code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,13 +12,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *temp, *new_node;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
 	temp = *head;
 
@@ -27,11 +36,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (count == idx - 1)
 		{
 			new_node->next = temp->next;
-			new_node->n = n;
 			temp->next = new_node;
+			return (new_node);
 		}
 		temp = temp->next;
 		count++;
 	}
-	return (new_node);
+	return (NULL);
 }
