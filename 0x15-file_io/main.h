3176949a01e47705ebedb78fda80d98d23e33e39
Gideon Bature <infoaboutgideon@gmail.com>
2023-07-11 05:58:05 +0100

    a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,10 +7,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stddef.h>
+#include <stdint.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error_and_exit(int exit_code, const char *error_message);
+void print_hex(uint8_t *buffer, size_t size);
+void print_elf_header(uint8_t *buffer);
 
 #endif
