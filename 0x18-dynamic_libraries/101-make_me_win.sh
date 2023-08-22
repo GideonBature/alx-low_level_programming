2cf286af8ca0ecd2a572349bf1742fcda6d4b1d9
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-22 07:19:06 +0100

    adjust file to print to standard error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -shared -o lib_test.so -fPIC test.c
-LD_PRELOAD=/home/bene/Desktop/alx-low_level_programming/0x18-dynamic_libraries/lib_test.so ./gm 9 8 10 24 75 9
+LD_PRELOAD=/home/bene/Desktop/alx-low_level_programming/0x18-dynamic_libraries/lib_test.so ./gm "9 8 10 24 75 9"
