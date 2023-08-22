22cec020c9dee2001df83a09765f7cb4c6b78484
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-22 04:49:23 +0100

    a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/lib_calc.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds a and b
+ * @a: first para
+ * @b: second para
+ *
+ * Return: an int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts a and b
+ * @a: first para
+ * @b: second para
+ *
+ * Return: an int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies a and b
+ * @a: first para
+ * @b: second para
+ *
+ * Return: an int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides a and b
+ * @a: first para
+ * @b: second para
+ *
+ * Return: an int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns reminder of a divide by b
+ * @a: first para
+ * @b: second para
+ *
+ * Return: an int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
