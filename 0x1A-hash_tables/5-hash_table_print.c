86877f42a323598b6868005e9e46ec49a17797af
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-08 16:06:25 +0100

    a function that prints a hash table.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,17 +8,24 @@
 */
 void hash_table_print(const hash_table_t *ht)
 {
-	int i = 0;
+	unsigned long int i = 0;
+	char *seperator = NULL;
 
 	if (ht)
 	{
+		printf("{");
 		while (i < ht->size)
 		{
 			while (ht->array[i])
 			{
+				if (seperator)
+					printf("%s", seperator);
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 				ht->array[i] = ht->array[i]->next;
+				seperator = ", ";
 			}
 			i++;
 		}
+		printf("}\n");
 	}
 }
