d1566e4b5ee900a61b1507a7eed7b69de180528a
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-08 01:31:17 +0100

    adjust check for size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = NULL;
 	unsigned long int i;
 
-	if (size < 0)
-		return (NULL);
-
 	table = malloc(sizeof(hash_table_t));
 
 	if (!table)
+		return (NULL);
+
+	if (size < 0)
 		return (NULL);
 
 	table->size = size;
