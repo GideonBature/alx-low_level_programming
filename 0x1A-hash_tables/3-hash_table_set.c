f61c7d99e23a52d49c5fafd9644af2b5a67b70e2
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-08 14:50:08 +0100

    restructure code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,9 +18,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	value_copy = strdup(value);
-	key_copy = strdup(key);
+	if (!value_copy)
+		return (0);
 
-	if (!value_copy || !key_copy)
+	key_copy = strdup(key);
+	if (!key_copy)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -31,6 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index]->value = value_copy;
 		return (1);
 	}
+
 	new_node = malloc(sizeof(hash_node_t));
 
 	if (!new_node)
