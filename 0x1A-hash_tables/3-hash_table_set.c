562fc77dafb77eafd831b6123c910c4c4e631d5e
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-08 14:33:47 +0100

    a function that adds an element to the hash table.

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds element to hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value of key
+ *
+ * Return: 0(failure) or 1(success)
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	char *value_copy = NULL, *key_copy = NULL;
+	hash_node_t *new_node = NULL;
+
+	if (!ht || !key)
+		return (0);
+	
+	value_copy = strdup(value);
+	key_copy = strdup(key);
+
+	if (!value_copy || !key_copy)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] && *(ht->array[index]->key) == *key)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = value_copy;
+		return (1);
+	}
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (!new_node)
+	{
+		free(value_copy);
+		free(key_copy);
+		return (0);
+	}
+
+	new_node->key = key_copy;
+	new_node->value = value_copy;
+	new_node->next = NULL;
+
+	if (ht->array[index])
+	{
+		new_node = ht->array[index];
+	}
+	ht->array[index] = new_node;
+	return (1);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,6 +32,7 @@ typedef struct hash_table_s
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
