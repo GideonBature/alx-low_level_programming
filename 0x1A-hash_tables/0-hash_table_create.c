b6e329ea2cb619cf42cc349d1deb4a570b2e38e6
Gideon Bature <infoaboutgideon@gmail.com>
2023-09-08 01:36:10 +0100

    adjust code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,15 +16,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * table->size);
+	table->array = malloc(sizeof(hash_node_t) * size);
 
 	if (!table->array)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
+	table->size = size;
+
 	return (table);
 }
