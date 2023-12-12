4b39d69f00028f3ab351ae293bfdd38c1753d88e
GideonBature <infoaboutgideon@gmail.com>
2023-12-12 13:55:16 +0100

    fix code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,7 +12,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t i, start = 0, step = sqrt(size), end = 0;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (array[end] < value && end <= size)
