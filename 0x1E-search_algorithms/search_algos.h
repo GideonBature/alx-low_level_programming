8181f4b5f8103815ac612988816d56a1bdd3b89a
GideonBature <infoaboutgideon@gmail.com>
2023-12-12 10:27:05 +0100

    add <math.h> header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
