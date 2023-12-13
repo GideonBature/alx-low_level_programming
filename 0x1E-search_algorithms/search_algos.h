a53aae55b04d85cf8d701f84ab4e7e1eeec4eeb5
GideonBature <infoaboutgideon@gmail.com>
2023-12-13 02:38:58 +0100

    fix code

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,6 +14,7 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 void print_array_elems(int *array, size_t left, size_t right);
 int binary_search_0(int *array, size_t low, size_t high, int value);
+size_t min(size_t x, size_t y);
 /* listint_t *jump_list(listint_t *list, size_t size, int value); */
 /* skiplist_t *linear_skip(skiplist_t *list, int value); */
 
